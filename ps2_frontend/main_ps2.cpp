80818f2476bacd7d24b26dfe6fe9a3d4b2fada6d
VicMisael <misael1805@gmail.com>
2026-08-28 22:44:12 -0300

    add frontends

--- ps2_frontend/main_ps2.cpp
+++ ps2_frontend/main_ps2.cpp
@@ -1,16 +1,19 @@
+#include "ps2_emulator.h"
 #include "rom_selector.h"
 
 #include <SDL.h>
 #include <ps2_filesystem_driver.h>
 
 #include <cstdio>
+#include <exception>
+#include <iostream>
 
 int main(int, char**)
 {
-    std::printf("PS2 GbSunny\n");
+    std::cout << "PS2 GbSunny" << std::endl;
     init_ps2_filesystem_driver();
 
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) != 0) {
         std::printf("SDL_Init failed: %s\n", SDL_GetError());
         deinit_ps2_filesystem_driver();
         return 1;
@@ -34,7 +37,7 @@ int main(int, char**)
     SDL_Renderer* renderer = SDL_CreateRenderer(
         window,
         -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        SDL_RENDERER_ACCELERATED );
 
     if (renderer == nullptr) {
         std::printf("SDL_CreateRenderer failed: %s\n", SDL_GetError());
@@ -44,15 +47,26 @@ int main(int, char**)
         return 1;
     }
 
-    const auto rom_path = ps2_frontend::select_rom(renderer);
-    if (rom_path.has_value()) {
-        std::printf("Selected ROM: %s\n", rom_path->c_str());
+    bool running = true;
+    while (running) {
+        const auto rom_path = ps2_frontend::select_rom(renderer);
+        if (!rom_path.has_value()) {
+            break;
+        }
+
+        std::cout << "Selected ROM: " << *rom_path << '\n';
+        try {
+            running = ps2_frontend::run_emulator(renderer, *rom_path)
+                == ps2_frontend::EmulatorResult::SelectRom;
+        } catch (const std::exception& error) {
+            std::cout << "Failed to run ROM: " << error.what() << '\n';
+        }
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    std::printf("Exiting to Console");
-    exit(0);
-    return rom_path.has_value() ? 0 : 1;
+    std::cout << "Exiting to console" << std::endl;
+
+    return 0;
 }
